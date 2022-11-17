0ff68e27c0a83b67bfcbe32cff5e5cce1a287346
salamatab <116481261+salamatab@users.noreply.github.com>
2022-11-17 12:08:45 +0100

    Add files via upload

---
+++ qui sommes nous.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML> <br/>
+<html>
+     <head>
+          <!-- en tete de page -->
+          <meta charset="UTF-8"/>
+          <title>qui somme nous</title>
+     </head>
+
+     <body> 
+          <h2>Qui sommes nous</h2>
+           <p class="introduction">safa-marwa c'est deux soeur qui proposent des bien et services vendue a la communauté ici en Belgique et en guinée. nos engagements est de soutenir des associations a but non lucratif qui défend des causes humanitaires, qui on vocation d'aider les enfants en Guinée et partout dans le monde en situation de précarité, handicap ou d'education. Nous sommes fier aujaurd'hui de pouvoir participer a de nombreuse actions que c'est association mettent en place.</p> 
+
+          <h2>Horaires d'ouvertures</h2>  
+
+     <table border="1px">
+          <tr>
+            <th>lundi</th>
+            <th>09:00 - 18:00</th>
+          </tr>
+          <tr>
+            <th>mardi</th>
+            <th>09:00 - 18:00</th>
+          </tr>
+          <tr>
+            <th>mercredi</th>
+            <th>09:00 - 18:00</th>
+          </tr>  
+          <tr>
+            <th>Jeudi</th>
+            <th>09:00 - 18:00</th>
+          </tr>
+          <tr>
+            <th>Vendre</th>
+            <th>09:00 - 18:00</th>
+          </tr>
+          <tr>
+            <th>Samedi</th>
+            <th>09:00 - 18:00</th>
+          </tr>  
+          <tr>
+               <th>Dimanche</th>
+               <th>Fermer</th>
+          </tr>
+     </table>
+     
+          <p><a href="index.php"target="_self">acceuil</a></p>
+          <p><a href="produits.php"target="_self">produits</a></p>
+          <p><a href="qui sommes nous.php"target="_self">qui sommes nous</a></p>
+       </body>
